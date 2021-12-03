bea148013e08e451b525b5be9b989f3a9e98510c
Kishan <kishan.sutex.kp@gmail.com>
2021-12-03 21:22:28 +0530

    init commite

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,14 @@
 class DemoController < ApplicationController
+
+  layout = false
+
   def index
+    @array = [1,2,3,4,5,6]
   end
+
+  def hello
+    @id = params['id']
+    @page = params[:page].to_i
+  end
+
 end
